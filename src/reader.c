fb799d57948fbf6a62d2443c6fb7b76ce458c7c3
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2014-10-10 19:19:51 +0100

    Added zip rule to Makefile. Removed printf calls. gitignore

--- src/reader.c
+++ src/reader.c
@@ -14,20 +14,17 @@ int reader(int file_num){
     int fd, file_value;
     sprintf(file_name, FILENAME, file_num); // place file_num in FILENAME
     fd = open(file_name, reader_f_flags);
-    
+
     if (fd == -1){ // error opening the file (does not exist?)
         return FILE_IS_INVALID;
     }
-    
-    // TEST PRINTFS BELOW
+
     if ( file_contents_are_valid(fd, WRITER_STRING_LEN, LINES_PER_FILE) == TRUE ){
-        printf("VALID!\n");
         file_value = FILE_IS_VALID;
     }else{
-        printf("INVALID!\n");
         file_value =  FILE_IS_INVALID;
     }
-    
+
     close(fd);
     return file_value;
 }
@@ -37,9 +34,9 @@ int file_contents_are_valid(int fd, int line_length, int line_count)
 {
     // number of bytes to read at a time from the file
     int line_size = line_length * sizeof(char);
-    
+
     char *first_line = (char*) malloc(line_size);
-    
+
     // try to read and validate the first line of the file
     if ( read(fd, first_line, line_size) != line_size ) {
         // the first line doesn't even have the correct number of characters
@@ -50,10 +47,10 @@ int file_contents_are_valid(int fd, int line_length, int line_count)
         free(first_line);
         return FALSE; // file is invalid
     }
-    
+
     // the first line is valid, so we can compare it with all other lines
     char *line_buffer = (char*) malloc(line_size);
-    
+
     // compare all lines with the first line
     int i;
     for ( i = 1 ; read(fd, line_buffer, line_size) == line_size; i++ )
@@ -64,24 +61,24 @@ int file_contents_are_valid(int fd, int line_length, int line_count)
             return FALSE; // file is invalid
         }
     }
-    
+
     free(first_line);
     free(line_buffer);
-    
+
     if (i != line_count)
         return FALSE;
-    
+
     return TRUE; // file is valid
 }
 
 int known_writer_string(char *str, int str_size){
     int i;
-    
+
     for ( i = 0; i < WRITER_STRING_COUNT; i++ ){
         if ( strncmp(str, writer_strings[i], str_size) == 0 ){
             return TRUE;
         }
     }
-    
+
     return FALSE;
 }
--- src/writer_main.c
+++ src/writer_main.c
@@ -1,18 +1,19 @@
 #include "writer.h"
 #include <time.h>
 #include "shared_stuff.h"
+#include <stdlib.h>
 
 int main (int argc, char *argv[]){
     int i, file_num;
     char *rand_str;
-    
+
     srand (time(NULL));
-    
+
     for ( i = 0; i < CYCLE_COUNT; i++ ){
     	file_num = RANDOM_RANGE(0, 4);
     	rand_str = get_writer_string( RANDOM_RANGE(0, WRITER_STRING_COUNT - 1) );
 		writer(file_num, rand_str, WRITER_STRING_LEN);
     }
-    
+
     return 0;
 }
