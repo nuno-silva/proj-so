a8b9ca22c41aca75664ca9da48e0edb4b0ddbaf1
ILUXONCHIK <iluxon4ik@hotmail.com>
2014-11-06 18:23:58 +0000

    [Keyaku] Brought appropriate CHILDREN_COUNT values to each reader and writer; Fixed typos

--- src/shared_stuff.h
+++ src/shared_stuff.h
@@ -1,33 +1,31 @@
 #ifndef __SHARED_STUFF_H__
-    #define __SHARED_STUFF_H__
-    
-    #include <stdio.h>
-    
-    // enables debug prints if defined; we define it in the makefile
-    //#define DEBUG
-    
-    
-    #define CYCLE_COUNT 5120
-    #define CHILDREN_COUNT 10
-    #define WRITER_MAIN_PATH  "./writer"
-    
-    #define WRITER_STRING_LEN   10
-    #define WRITER_STRING_COUNT 10
-    #define LINES_PER_FILE      1024
-    
-    #define FILENAME "SO2014-%d.txt"
-    
-    #define RANDOM_RANGE(MIN, MAX) (( rand() % ( MAX + 1 - MIN ) ) + MIN )
-    
-    // used to print debug messages
-    #ifdef DEBUG
-        #define DBG_PRINTF(format, ...) printf("%s:%d: "format, __FILE__, __LINE__, __VA_ARGS__)
-    #else
-        #define DBG_PRINTF(format, ...) 
-    #endif
-    
-    extern char writer_strings [ WRITER_STRING_COUNT ][ WRITER_STRING_LEN + 1 ];
-    
-    #define TRUE    1
-    #define FALSE   0
+	#define __SHARED_STUFF_H__
+	
+	#include <stdio.h>
+	
+	// enables debug prints if defined; we define it in the makefile
+	//#define DEBUG
+	
+	#define CYCLE_COUNT 5120
+	#define WRITER_MAIN_PATH  "./writer"
+	
+	#define WRITER_STRING_LEN   10
+	#define WRITER_STRING_COUNT 10
+	#define LINES_PER_FILE      1024
+	
+	#define FILENAME "SO2014-%d.txt"
+	
+	#define RANDOM_RANGE(MIN, MAX) (( rand() % ( MAX + 1 - MIN ) ) + MIN )
+	
+	// used to print debug messages
+	#ifdef DEBUG
+	    #define DBG_PRINTF(format, ...) printf("%s:%d: "format, __FILE__, __LINE__, __VA_ARGS__)
+	#else
+	    #define DBG_PRINTF(format, ...) 
+	#endif
+	
+	extern char writer_strings[ WRITER_STRING_COUNT ][ WRITER_STRING_LEN + 1 ];
+	
+	#define TRUE    1
+	#define FALSE   0
 #endif
--- src/writer.h
+++ src/writer.h
@@ -1,18 +1,26 @@
 #ifndef __WRITER_H__
-    #define __WRITER_H__
-
-    /**
-     * @brief Opens the file specified by file_num and writes to it.
-     * @param file_nume the number of the file
-     * @param txt pointer to the string to write
-     * @param txt_len number of characters in txt to write
-     */
-    void writer(int file_num, char *txt, int txt_len);
-
-
-    /**
-     * @brief Get a writer string by its index
-     */
-    char *get_writer_string( int index );
+	#define __WRITER_H__
+	
+	
+	#define CHILDREN_COUNT 10	/*	Constant useful only for writer function	*/
+	
+	/**
+	 * @brief Opens the file specified by file_num and writes to it.
+	 * 
+	 * @param file_num the number of the file
+	 * @param txt pointer to the string to write
+	 * @param txt_len number of characters in txt to write
+	 */
+	void writer(int file_num, char *txt, int txt_len);
+	
+	
+	/**
+	 * @brief Get a writer string by its index
+	 * 
+	 * @param index string index 
+	 * 
+	 * @retval string placed in the index position of the global value writer_strings
+	 */
+	char *get_writer_string(int index);
 
 #endif //__WRITER_H__
