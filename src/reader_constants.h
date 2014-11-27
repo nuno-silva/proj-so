30e2907e83f70f7e0766906120973450e0086ca7
Keyaku <antonio.luis.sarmento@gmail.com>
2014-11-27 02:26:32 +0000

    Preparations for Reader Parent
    Created prototype version "reader_parent_main.c" highly based upon "reader_parent_main_3.c"; Adapted .gitignore, Makefile, reader_constants to meet the new requirements.

--- src/reader_constants.h
+++ src/reader_constants.h
@@ -1,15 +1,10 @@
 #ifndef __READER_CONSTANTS_H__
 	#define __READER_CONSTANTS_H__
-	
-	
-	#define READER_1_CHILDREN_COUNT	3
-	
-	#define READER_2_THREAD_COUNT	3
-	
-	/*	Constant K for Exercise 4 pdf	*/
-	#define READER_3_THREAD_COUNT	7	
-	
+
+
+	#define READER_THREAD_COUNT 3	
+
 	#define FILE_IS_VALID	 0
 	#define FILE_IS_INVALID	-1
-	
+
 #endif
