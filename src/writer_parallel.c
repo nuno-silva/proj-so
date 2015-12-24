864a0a92bf5754f3c0fde84f7164576fa699c351
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2015-12-24 01:40:09 +0000

    Added license header to source files.

--- src/writer_parallel.c
+++ src/writer_parallel.c
@@ -1,3 +1,19 @@
+/*
+ * This file is part of proj-so.
+ *
+ * Copyright (C) 2014 Antonio Sarmento, Illya Gerasymchuk, Nuno Silva. All
+ * Rights Reserved.
+ *
+ * proj-so is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * proj-so is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
